1fc3675b9396e233c6e358099927b96d9c1af21c
IngoSternberg <ingosternbergfau@gmail.com>
2024-09-03 14:56:09 +0200

    :tada: adjust project structure

---
+++ backend/README.md
@@ -0,0 +1,14 @@
+# Kalah Backend
+
+
+```
+mvn wrapper:wrapper
+```
+
+```
+mvn package
+```
+
+```
+./mvnw spring-boot:run
+```
---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import {Routes} from '@angular/router';
+import {StartPageComponent} from "./pages/start-page/start-page.component";
+import {GamePageComponent} from "./pages/game-page/game-page.component";
+
+export const routes: Routes = [
+    {
+        path: 'start',
+        component: StartPageComponent
+    },
+    {
+        path: 'play',
+        component: GamePageComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'start'
+    }
+];

4e050cdecf31d94711b36e6b63ad1e560a7c46ad
IngoSternberg <ingosternbergfau@gmail.com>
2024-09-03 16:14:12 +0200

    :sparkles: first Spring Steps

--- backend/src/main/java/com/example/kalah/HelloController.java
+++ backend/src/main/java/com/example/kalah/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package com.example.kalah;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

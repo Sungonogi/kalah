01d397683ef21d66cc8778e050308f89f006301d
IngoSternberg <ingosternbergfau@gmail.com>
2024-09-04 20:58:09 +0200

    :sparkles: websocket test

---
+++ backend/src/main/java/com/example/kalah/WebSocketController.java
@@ -0,0 +1,43 @@
+package com.example.kalah;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+record GreetingRequest(String name) {
+}
+
+record GreetingResponse(String message) {
+}
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/chat")
+    @SendTo("/answer")
+    GreetingResponse greet(GreetingRequest request) {
+        return new GreetingResponse("Hello, " + request.name());
+    }
+}
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/answer");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").withSockJS();
+    }
+}

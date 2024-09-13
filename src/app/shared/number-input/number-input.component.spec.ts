113d128a20b76a0851baa2cb9002f65bb849effd
IngoSternberg <ingo.sternberg@fau.de>
2024-09-13 17:41:58 +0200

    :green_heart: fix tests and thus github actions?

--- src/app/shared/number-input/number-input.component.spec.ts
+++ src/app/shared/number-input/number-input.component.spec.ts
@@ -1,7 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 import {NumberInputComponent} from './number-input.component';
-import {NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 describe('NumberInputComponent', () => {
     let component: NumberInputComponent;

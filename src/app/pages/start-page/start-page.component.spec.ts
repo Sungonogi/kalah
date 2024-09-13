ce79b3affd58ebd3825e7d4d52a493eacf86d3e0
IngoSternberg <ingo.sternberg@fau.de>
2024-09-13 17:10:08 +0200

    :white_check_mark: fix tests

--- src/app/pages/start-page/start-page.component.spec.ts
+++ src/app/pages/start-page/start-page.component.spec.ts
@@ -1,4 +1,5 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 import {StartPageComponent} from './start-page.component';
 
@@ -8,7 +9,7 @@ describe('StartPageComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [StartPageComponent]
+            imports: [StartPageComponent, NoopAnimationsModule]
         })
             .compileComponents();
 

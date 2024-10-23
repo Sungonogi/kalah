4dc2d256ffe2dc55d58ded99540c07e1afeed180
Ingo Sternberg <ingo.sternberg@fau.de>
2024-10-23 23:41:29 +0200

    :white_check_mark: fix the test

--- src/app/guards/navi-warning.guard.spec.ts
+++ src/app/guards/navi-warning.guard.spec.ts
@@ -1,11 +1,14 @@
 import {TestBed} from '@angular/core/testing';
 import {CanDeactivateFn} from '@angular/router';
 
+import {GamePageComponent} from "../pages/game-page/game-page.component";
 import {naviWarningGuard} from './navi-warning.guard';
 
 describe('naviWarningGuard', () => {
-    const executeGuard: CanDeactivateFn = (...guardParameters) =>
-        TestBed.runInInjectionContext(() => naviWarningGuard(...guardParameters));
+    // @ts-expect-error - it does not matter as it just takes the component as parameter
+    const executeGuard: CanDeactivateFn = (component: GamePageComponent) =>
+        // @ts-expect-error - see above
+        TestBed.runInInjectionContext(() => naviWarningGuard(component));
 
     beforeEach(() => {
         TestBed.configureTestingModule({});

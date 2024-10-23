c7dc87a710f50578aae9298b6ea85c8e4a5597b6
Ingo Sternberg <ingo.sternberg@fau.de>
2024-10-23 23:44:00 +0200

    :white_check_mark: fix the test

--- src/app/pages/game-page/warning-dialog/warning-dialog.component.spec.ts
+++ src/app/pages/game-page/warning-dialog/warning-dialog.component.spec.ts
@@ -1,14 +1,22 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 
 import {WarningDialogComponent} from './warning-dialog.component';
 
 describe('WarningDialogComponent', () => {
     let component: WarningDialogComponent;
     let fixture: ComponentFixture<WarningDialogComponent>;
+    const mockDialogData = {
+        title: 'Warning',
+        message: 'Are you sure you want to quit?'
+    };
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [WarningDialogComponent]
+            imports: [WarningDialogComponent],
+            providers: [
+                {provide: MAT_DIALOG_DATA, useValue: mockDialogData}
+            ]
         })
             .compileComponents();
 

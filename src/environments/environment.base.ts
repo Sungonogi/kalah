420fcce6e9c44ba938a4ac2de08efc625da19628
IngoSternberg <ingosternbergfau@gmail.com>
2025-03-05 19:31:48 +0100

    💡 add comment

--- src/environments/environment.base.ts
+++ src/environments/environment.base.ts
@@ -1,5 +1,5 @@
 export const baseEnvironment = {
-    engineTimeLimit: 50,
+    engineTimeLimit: 50, // good enough and I don't want to take too much CPU time, also this is a lower bound and it can take longer
     minimumWaitTime: 1000, // if the computer is too fast, wait at least this long
     disableAudio: false,
     gameOverAnimationTime: 800,

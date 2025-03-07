db686a538a4a4f463866169c0b6c064f021358b8
IngoSternberg <ingosternbergfau@gmail.com>
2025-03-07 11:12:02 +0100

    🔧 adjust config

--- src/environments/environment.base.ts
+++ src/environments/environment.base.ts
@@ -3,6 +3,6 @@ export const baseEnvironment = {
     minimumWaitTime: 1000, // if the computer is too fast, wait at least this long
     disableAudio: false,
     gameOverAnimationTime: 800,
-    gameOverModalTime: 200,
+    gameOverModalTime: 600,
     gameStartTime: 1000,
 };
